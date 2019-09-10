fdbae99b1c05e67ea0e665adcf9e1665799722c7
RyfiGaming <55142212+RyfiGaming@users.noreply.github.com>
2019-09-10 16:48:16 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -55,4 +55,4 @@ bot.on('message', async message => {
     bot.rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     bot.uId = message.author.id;
 });
-bot.login('BOT_TOKEN');
+bot.login(process.env.BOT_TOKEN);
